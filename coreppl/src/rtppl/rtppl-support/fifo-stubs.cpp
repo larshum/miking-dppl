e2071adcceea3aa9447cde3b73680794d9d98cf2
larshum <larshum@kth.se>
2023-04-19 17:41:08 +0200

    Add distributions in RTPPL support library

--- coreppl/src/rtppl/rtppl-support/fifo-stubs.cpp
+++ coreppl/src/rtppl/rtppl-support/fifo-stubs.cpp
@@ -9,104 +9,90 @@
 #include <iostream>
 #include <vector>
 
-template <typename T>
 struct payload {
-  int64_t ts;
-  T val;
+  char *data;
+  int64_t size;
 };
 
-struct dist {
-  int64_t nsamples;
-  bool degenerate;
-  double normConst;
-  std::vector<double> log_weights;
-  std::vector<double> samples;
-  std::vector<double> weights;
-};
-
-template <typename T>
-std::vector<payload<T>> read_messages(int fd) {
-  std::vector<payload<T>> input_seq;
-  while (true) {
-    payload<T> msg;
-    int elems = read(fd, (void*)&msg, sizeof(payload<T>));
-    if (elems > 0) {
-      input_seq.push_back(msg);
-    } else {
-      break;
-    }
-  }
-  return input_seq;
+int min(int a, int b) {
+  if (a < b) return a;
+  return b;
 }
 
-std::vector<payload<dist>> read_dist_messages(int fd, int64_t nfields) {
-  std::vector<payload<dist>> input_seq;
-  while (true) {
-    payload<dist> input;
-    int elems = read(fd, (void*)&input.ts, sizeof(int64_t));
-    if (elems == 0) break;
-    elems = read(fd, (void*)&input.val.nsamples, sizeof(int64_t));
-    if (elems == 0) break;
-    elems = read(fd, (void*)&input.val.degenerate, sizeof(int64_t));
-    if (elems == 0) break;
-    elems = read(fd, (void*)&input.val.normConst, sizeof(double));
-    if (elems == 0) break;
-    // For each sample of a distribution, we have one log-weight and one
-    // weight, as well as one float for each element in the record.
-    int64_t n = (nfields + 2) * input.val.nsamples;
-    double buffer[n];
-    elems = read(fd, (void*)&buffer, sizeof(buffer));
-    if (elems != n) break;
-    double *samples_offset = buffer + input.val.nsamples;
-    double *weights_offset = samples_offset + nfields * input.val.nsamples;
-    input.val.log_weights = std::vector<double>(buffer, samples_offset);
-    input.val.weights = std::vector<double>(samples_offset, weights_offset);
-    input.val.samples = std::vector<double>(weights_offset, buffer + n);
-    input_seq.emplace_back(input);
-  }
-  return input_seq;
-}
-
-int64_t write_dist_float_record_buffer(value d, char *buffer, int64_t nfields) {
-  value degen = Field(d, 0);
-  value norm_const = Field(Field(d, 1), 0);
-  value log_weights = Field(d, 2);
-  value samples = Field(d, 3);
-  value weights = Field(d, 4);
-  int64_t nsamples = Wosize_val(log_weights);
-  // The buffer contains the timestamp plus all fields required to encode a
-  // distribution.
-  size_t sz = (nfields + 2) * nsamples + 3 * sizeof(int64_t) + sizeof(double);
-  buffer = (char*)malloc(sz);
-  char *nsamples_offset = buffer + sizeof(int64_t);
-  char *degen_offset = nsamples_offset + sizeof(int64_t);
-  char *nc_offset = degen_offset + sizeof(int64_t);
-  memcpy(buffer, &nsamples, sizeof(int64_t));
-  int64_t degen_tmp = Bool_val(degen);
-  memcpy(degen_offset, &degen_tmp, sizeof(int64_t));
-  double nc_tmp = Double_val(norm_const);
-  memcpy(nc_offset, &nc_tmp, sizeof(double));
-  char *ptr = nc_offset + sizeof(double);
-  double tmp;
-  for (size_t i = 0; i < nsamples; i++) {
-    tmp = Double_val(Field(log_weights, i));
-    memcpy(ptr, &tmp, sizeof(double));
-    ptr += sizeof(double);
-  }
-  for (size_t i = 0; i < nsamples; i++) {
-    tmp = Double_val(Field(weights, i));
-    memcpy(ptr, &tmp, sizeof(double));
-    ptr += sizeof(double);
-  }
-  for (size_t i = 0; i < nsamples; i++) {
-    value rec = Field(samples, i);
-    for (size_t j = 0; j < nfields; j++) {
-      tmp = Double_val(Field(rec, j));
-      memcpy(ptr, &tmp, sizeof(double));
-      ptr += sizeof(double);
+int64_t read_message_size(int fd) {
+  char buffer[sizeof(int64_t)];
+  int64_t count = 0;
+  while (count < sizeof(int64_t)) {
+    int bytes = read(fd, (void*)&buffer[count], sizeof(int64_t)-count);
+    if (bytes < 0) {
+      if (count > 0 && errno == EAGAIN) continue;
+      return -1;
     }
+    count += bytes;
   }
+  int64_t sz;
+  memcpy((void*)&sz, buffer, sizeof(int64_t));
   return sz;
+}
+
+int64_t read_message(int fd, payload& p) {
+  p.size = read_message_size(fd);
+  if (p.size == -1) {
+    return false;
+  }
+  p.data = (char*)malloc(p.size);
+  int64_t count = 0;
+  while (count < p.size) {
+    int bytes_read = read(fd, (void*)&p.data[count], p.size - count);
+    if (bytes_read <= 0) {
+      if (errno == EAGAIN) continue;
+      fprintf(stderr, "Error reading message: %s\n", strerror(errno));
+      exit(1);
+    }
+    count += bytes_read;
+  }
+  return true;
+}
+
+std::vector<payload> read_messages(int fd) {
+  std::vector<payload> input_seq;
+  payload p;
+  while (read_message(fd, p)) {
+    input_seq.emplace_back(p);
+  }
+  return input_seq;
+}
+
+bool write_message_size(int fd, int64_t sz) {
+  char buffer[sizeof(int64_t)];
+  memcpy((void*)buffer, (void*)&sz, sizeof(int64_t));
+  int64_t count = 0;
+  while (count < sizeof(int64_t)) {
+    int bytes = write(fd, (void*)&buffer[count], sizeof(int64_t)-count);
+    if (bytes < 0) {
+      if (errno == EAGAIN) continue;
+      return false;
+    }
+    count += bytes;
+  }
+  return true;
+}
+
+void write_message(int fd, const payload& p) {
+  bool res = write_message_size(fd, p.size);
+  if (!res) {
+    fprintf(stderr, "Error writing message: %s\n", strerror(errno));
+    exit(1);
+  }
+  int64_t count = 0;
+  while (count < p.size) {
+    int bytes_written = write(fd, (void*)&p.data[count], p.size - count);
+    if (bytes_written <= 0) {
+      fprintf(stderr, "Error writing message: %s\n", strerror(errno));
+      exit(1);
+    }
+    count += bytes_written;
+  }
 }
 
 value to_timespec_value(int64_t ts) {
@@ -124,109 +110,158 @@ int64_t timespec_value_to_int64(value ts) {
   return sec * (int64_t)1e9 + nsec;
 }
 
-value to_float_array_value(const std::vector<double>& v) {
-  value rec = caml_alloc(v.size(), 0);
-  for (size_t i = 0; i < v.size(); i++) {
-    Store_field(rec, i, caml_copy_double(v[i]));
-  }
-  return rec;
+value read_float_payload(const payload& p) {
+  int64_t ts;
+  memcpy(&ts, (char*)p.data, sizeof(int64_t));
+  value timespec = to_timespec_value(ts);
+  value tsv = caml_alloc(2, 0);
+  Store_field(tsv, 0, timespec);
+  double val;
+  memcpy(&val, (char*)p.data + sizeof(int64_t), sizeof(double));
+  Store_field(tsv, 1, caml_copy_double(val));
+  free(p.data);
+  return tsv;
 }
 
-value to_dist_float_record_value(const dist& d) {
-  // TODO(larshum, 2023-04-16): This will break if more distributions are added
-  // in DPPL. Can we figure out the value of this tag in a safe way?
-  const int EMPIRICAL_DIST_TAG = 11;
-  value dist = caml_alloc(5, EMPIRICAL_DIST_TAG);
-  Store_field(dist, 0, Val_bool(d.degenerate));
-  // TODO(larshum, 2023-04-16): This breaks if using an inference algorithm
-  // that does not contain a single double argument.
-  value emp_norm = caml_alloc(1, 0);
-  Store_field(emp_norm, 0, caml_copy_double(d.normConst));
-  Store_field(dist, 1, emp_norm);
-  Store_field(dist, 2, to_float_array_value(d.log_weights));
-  Store_field(dist, 3, to_float_array_value(d.samples));
-  Store_field(dist, 4, to_float_array_value(d.weights));
-  return dist;
+payload write_float_payload(value msg, value ts) {
+  payload p;
+  p.size = sizeof(int64_t) + sizeof(double);
+  p.data = (char*)malloc(p.size);
+  int64_t timestamp = timespec_value_to_int64(ts);
+  memcpy(p.data, (void*)&timestamp, sizeof(int64_t));
+  double data = Double_val(msg);
+  memcpy(p.data + sizeof(int64_t), (void*)&data, sizeof(double));
+  return p;
+}
+
+value read_dist_float_record_payload(const payload& p, int64_t nfields) {
+  int64_t nsamples = (p.size - sizeof(int64_t)) / ((nfields + 1) * sizeof(double));
+  char *ptr = p.data;
+  int64_t timestamp;
+  memcpy((void*)&timestamp, ptr, sizeof(int64_t));
+  ptr += sizeof(int64_t);
+  value ts = to_timespec_value(timestamp);
+  value dist_samples = caml_alloc(nsamples, 0);
+  for (size_t i = 0; i < nsamples; i++) {
+    double weight;
+    memcpy((void*)&weight, ptr, sizeof(double));
+    value w = caml_copy_double(weight);
+    ptr += sizeof(double);
+    value s = caml_alloc(nfields, 0);
+    for (size_t j = 0; j < nfields; j++) {
+      double tmp;
+      memcpy((void*)&tmp, ptr, sizeof(double));
+      Store_field(s, j, caml_copy_double(tmp));
+      ptr += sizeof(double);
+    }
+    value sample = caml_alloc(2, 0);
+    Store_field(sample, 0, w);
+    Store_field(sample, 1, s);
+    Store_field(dist_samples, i, sample);
+  }
+  value tsv = caml_alloc(2, 0);
+  Store_field(tsv, 0, ts);
+  Store_field(tsv, 1, dist_samples);
+  return tsv;
+}
+
+payload write_dist_float_record_payload(value d, value ts, value nfields_val) {
+  value samples = Field(d, 0);
+  value log_weights = Field(d, 1);
+  int64_t nsamples = Wosize_val(samples);
+  int64_t nfields = Long_val(nfields_val);
+  payload p;
+  // The payload consists of a timestamp, log weights, and sample data
+  p.size = sizeof(int64_t) + nsamples * sizeof(double) + nsamples * nfields * sizeof(double);
+  p.data = (char*)malloc(p.size);
+  char *ptr = p.data;
+  int64_t timestamp = timespec_value_to_int64(ts);
+  memcpy(ptr, (void*)&timestamp, sizeof(int64_t));
+  ptr += sizeof(int64_t);
+  for (size_t i = 0; i < nsamples; i++) {
+    double tmp = Double_field(log_weights, i);
+    memcpy(ptr, (void*)&tmp, sizeof(double));
+    ptr += sizeof(double);
+    value rec = Field(samples, i);
+    for (size_t j = 0; j < nfields; j++) {
+      tmp = Double_val(Field(rec, j));
+      memcpy(ptr, (void*)&tmp, sizeof(double));
+      ptr += sizeof(double);
+    }
+  }
+  return p;
+}
+
+int open_pipe(value pipe, int flags) {
+  const char *pipe_id = String_val(pipe);
+  int fd = open(pipe_id, flags);
+  if (fd == -1) {
+    fprintf(stderr, "Error: could not open pipe %s\n", pipe_id);
+    exit(1);
+  }
+  return fd;
+}
+
+int open_pipe_nonblock(value pipe) {
+  return open_pipe(pipe, O_RDWR | O_NONBLOCK);
+}
+
+int open_pipe_blocking(value pipe) {
+  return open_pipe(pipe, O_RDWR);
 }
 
 extern "C" {
-
-  typedef payload<double> float_data_t;
-  typedef payload<dist> dist_record_float_data_t;
-
-  int open_pipe_file(value pipe) {
-    const char *pipe_id = String_val(pipe);
-    int fd = open(pipe_id, O_RDWR | O_NONBLOCK);
-    if (fd == -1) {
-      fprintf(stderr, "Error: could not open pipe %s\n", pipe_id);
-      exit(1);
-    }
-    return fd;
-  }
 
   value read_float_named_pipe_stub(value pipe) {
     CAMLparam1(pipe);
     CAMLlocal1(out);
 
-    int fd = open_pipe_file(pipe);
-    std::vector<float_data_t> input_seq = read_messages<double>(fd);
+    int fd = open_pipe_nonblock(pipe);
+    std::vector<payload> input_seq = read_messages(fd);
     close(fd);
 
     out = caml_alloc(input_seq.size(), 0);
     for (size_t i = 0; i < input_seq.size(); i++) {
-      value timespec = to_timespec_value(input_seq[i].ts);
-      value tsv = caml_alloc(2, 0);
-      Store_field(tsv, 0, timespec);
-      Store_field(tsv, 1, caml_copy_double(input_seq[i].val));
-      Store_field(out, i, tsv);
+      Store_field(out, i, read_float_payload(input_seq[i]));
     }
-    CAMLreturn(out);
-  }
-
-  value read_dist_float_record_named_pipe_stub(value pipe, value nfields) {
-    CAMLparam2(pipe, nfields);
-    CAMLlocal1(out);
-
-    int fd = open_pipe_file(pipe);
-    int64_t fields = Long_val(nfields);
-    std::vector<dist_record_float_data_t> input_seq = read_dist_messages(fd, fields);
-    close(fd);
-
-    out = caml_alloc(input_seq.size(), 0);
-    for (size_t i = 0; i < input_seq.size(); i++) {
-      value timespec = to_timespec_value(input_seq[i].ts);
-      value tsv = caml_alloc(2, 0);
-      Store_field(tsv, 0, timespec);
-      Store_field(tsv, 1, to_dist_float_record_value(input_seq[i].val));
-      Store_field(out, i, tsv);
-    }
-
     CAMLreturn(out);
   }
 
   void write_float_named_pipe_stub(value pipe, value msg, value ts) {
     CAMLparam3(pipe, msg, ts);
 
-    int fd = open_pipe_file(pipe);
-    float_data_t message;
-    message.ts = timespec_value_to_int64(ts);
-    message.val = Double_val(msg);
-    write(fd, (void*)&message, sizeof(float_data_t));
+    int fd = open_pipe_blocking(pipe);
+    payload p = write_float_payload(msg, ts);
+    write_message(fd, p);
+    free(p.data);
     close(fd);
 
     CAMLreturn0;
   }
 
+  value read_dist_float_record_named_pipe_stub(value pipe, value nfields_val) {
+    CAMLparam2(pipe, nfields_val);
+    CAMLlocal1(out);
+
+    int fd = open_pipe_nonblock(pipe);
+    std::vector<payload> input_seq = read_messages(fd);
+    close(fd);
+
+    int64_t nfields = Long_val(nfields_val);
+    out = caml_alloc(input_seq.size(), 0);
+    for (size_t i = 0; i < input_seq.size(); i++) {
+      Store_field(out, i, read_dist_float_record_payload(input_seq[i], nfields));
+    }
+    CAMLreturn(out);
+  }
+
   void write_dist_float_record_named_pipe_stub(value pipe, value msg, value ts, value nfields) {
     CAMLparam3(pipe, msg, ts);
 
-    int fd = open_pipe_file(pipe);
-    char *buffer;
-    int64_t n = write_dist_float_record_buffer(msg, buffer, Long_val(nfields));
-    int64_t t = timespec_value_to_int64(ts);
-    memcpy(buffer, &t, sizeof(int64_t));
-    write(fd, (void*)&buffer, n);
-    free(buffer);
+    int fd = open_pipe_blocking(pipe);
+    payload p = write_dist_float_record_payload(msg, ts, nfields);
+    write_message(fd, p);
+    free(p.data);
     close(fd);
 
     CAMLreturn0;
--- coreppl/src/rtppl/rtppl-support/rtppl.ml
+++ coreppl/src/rtppl/rtppl-support/rtppl.ml
@@ -31,6 +31,7 @@ let set_signal_handler (x : signal) (f : signal -> unit) : unit =
 
 type timespec = int * int
 type 'a tsv = timespec * 'a
+type opaque
 
 external clock_get_time : unit -> timespec = "clock_get_time_stub"
 external clock_nanosleep : timespec -> unit = "clock_nanosleep_stub"
@@ -40,6 +41,10 @@ external set_priority : int -> int = "set_priority_stub"
 let set_max_priority (_ : unit) : int = set_priority 255
 
 external read_float_named_pipe : string -> (float tsv) array = "read_float_named_pipe_stub"
-external read_dist_float_record_named_pipe : string -> (Obj.t tsv) array = "read_dist_float_record_named_pipe_stub"
 external write_float_named_pipe : string -> float -> timespec -> unit = "write_float_named_pipe_stub"
-external write_dist_float_record_named_pipe : string -> Obj.t -> timespec -> int -> unit = "write_dist_float_record_named_pipe_stub"
+external read_dist_float_record_named_pipe
+  : string -> int -> ((float * opaque) array tsv) array
+  = "read_dist_float_record_named_pipe_stub"
+external write_dist_float_record_named_pipe
+  : string -> opaque array * float array -> timespec -> int -> unit
+  = "write_dist_float_record_named_pipe_stub"
