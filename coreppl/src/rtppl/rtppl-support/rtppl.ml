6be492d984aa3753aa57aba5cad15a9f0f75d1c0
larshum <larshum@kth.se>
2023-04-20 10:04:18 +0200

    Clean up RTPPL support library

--- coreppl/src/rtppl/rtppl-support/rtppl.ml
+++ coreppl/src/rtppl/rtppl-support/rtppl.ml
@@ -1,29 +1,3 @@
-type time_stamped_value = int * Obj.t
-
-external lv_read_stub : int -> time_stamped_value = "lv_read_stub"
-external lv_write_stub : int -> time_stamped_value -> unit = "lv_write_stub"
-external lv_read_float_stub : int -> time_stamped_value = "lv_read_float_stub"
-external lv_write_float_stub : int -> time_stamped_value -> unit = "lv_write_float_stub"
-
-let lv_read (id : int) : time_stamped_value =
-  let (ts, value) = lv_read_stub id in
-  (ts, Marshal.from_bytes (Obj.obj value) 0)
-
-let lv_read_float (id : int) : time_stamped_value = lv_read_float_stub id
-
-let lv_write (id : int) (tsv : time_stamped_value) : unit =
-  let (ts, value) = tsv in
-  lv_write_stub id (ts, Obj.repr (Marshal.to_bytes value []))
-
-let lv_write_float (id : int) (tsv : time_stamped_value) : unit =
-  lv_write_float_stub id tsv
-
-let read_binary (ch : in_channel) : Obj.t =
-  Obj.repr (input_value ch)
-
-let write_binary (ch : out_channel) (data : Obj.t) : unit =
-  output_value ch (Obj.obj data)
-
 type signal = int
 
 let set_signal_handler (x : signal) (f : signal -> unit) : unit =
