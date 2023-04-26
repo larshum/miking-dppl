97650a53a256b210d6ac61c9effd56a67b28cd88
Lars Hummelgren <larshum@kth.se>
2023-04-26 17:16:29 +0200

    Fix bug in MacOS specific RTPPL support code

--- coreppl/src/rtppl/rtppl-support/rt-stubs.c
+++ coreppl/src/rtppl/rtppl-support/rt-stubs.c
@@ -71,7 +71,7 @@ inline void timespec_monodiff_rml(struct timespec *ts_out, const struct timespec
 inline int clock_nanosleep_abstime(const struct timespec *req) {
 #ifdef __MACH__
   struct timespec ts_delta;
-  int retval = clock_get_timespec(&ts_delta);
+  int retval = clock_get_monotonic_timespec(&ts_delta);
   if (retval == 0) {
     timespec_monodiff_rml(&ts_delta, req);
     retval = nanosleep(&ts_delta, NULL);
